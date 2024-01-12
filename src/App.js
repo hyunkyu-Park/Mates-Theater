ca32b6b20f86a5f9a4ef0113ab6e5721a50529b1
hyunkyu-Park <park620630@gmail.com>
2024-01-11 19:54:34 -0500

    using compoenents

--- src/App.js
+++ src/App.js
@@ -1,58 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
 import { Navbar, Container, Nav, Row, Col } from 'react-bootstrap';
+import bg1 from './img/bg1.avif'
 import m1 from './img/m1.jpeg'
 import m2 from './img/m2.jpeg'
 import m3 from './img/m3.jpg'
-
+import movie from './data'
+import { useState } from 'react';
 
 function App() {
+
+  let [movies, setMovies] = useState(movie)
+  let [images] = useState([m1, m2, m3])
+
   return (
     <div className="App">
       <Navbar bg="primary" data-bs-theme="dark">
         <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Navbar.Brand href="#home">Mates Theater</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link href="#home">Future</Nav.Link>
+            <Nav.Link href="#features">Present</Nav.Link>
+            <Nav.Link href="#pricing">Past</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
       <div className='main-bg'></div>
-
-      <div className="container">
-        <div className="row">
-          <div className="col-md-4">
-            <img src={m1} width="60%" />
-            <h4>Glass Onion: A Knives Out Mystery</h4>
-            <p className='description'>Tech billionaire Miles Bron invites his friends for a getaway on his private Greek island.
-              When someone turns up dead, Detective Benoit Blanc is put on the case</p>
-          </div>
-          <div className="col-md-4">
-            <img src={m2} width="60%" />
-            <h4>The Lord of the Rings: The Return of the King</h4>
-            <p className='description'>Frodo, Sam and Gollum are making their final way toward Mount Doom to destroy the One Ring,
-             unaware of Gollum's true intentions, while Merry, Pippin, Gandalf, Aragorn, Legolas, Gimli and the others 
-             join forces together against Sauron and his legions in Minas Tirith.</p>
-          </div>
-          <div className="col-md-4">
-            <img src={m3} width="60%" />
-            <h4>probability of love at first sight</h4>
-            <p className='description'>17-year-old Hadley misses her flight to London, 
-              where her father is marrying the woman he left their family for, 
-              but she unexpectedly may have found true love in the process.</p>
-          </div>
-        </div>
-      </div>
-
-
-
-
-
       
+      <div className='row'> 
+        <Movie images = {images} movies = {movies} post = {0}/>
+        <Movie images = {images} movies = {movies} post = {1}/>
+        <Movie images = {images} movies = {movies} post = {2}/>
+      </div>
     </div>
   );
+}
+
+function Movie(props){
+  return(
+    <div className="col-md-4">
+      <img src={props.images[props.post]} width="60%" />
+      <h4>{props.movies[props.post].title}</h4>
+      <p className='description'>{props.movies[props.post].content}</p>
+    </div>
+  )
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,24 @@
+let movie = [
+    {
+      id : 0,
+      title : "Glass Onion: A Knives Out Mystery",
+      content : "Tech billionaire Miles Bron invites his friends for a getaway on his private Greek island. When someone turns up dead, Detective Benoit Blanc is put on the case",
+      tags : ["Neflix", "Youtube", "Amazon"]
+    },
+  
+    {
+      id : 1,
+      title : "The Lord of the Rings: The Return of the King",
+      content : "Frodo, Sam and Gollum are making their final way toward Mount Doom to destroy the One Ring, unaware of Gollum's true intentions, while Merry, Pippin, Gandalf, Aragorn, Legolas, Gimli and the others  join forces together against Sauron and his legions in Minas Tirith",
+      tags : ["Youtube"]
+    },
+  
+    {
+      id : 2,
+      title : "probability of love at first sight",
+      content : "17-year-old Hadley misses her flight to London, where her father is marrying the woman he left their family for, but she unexpectedly may have found true love in the process.",
+      tags : ["Netflix", "Amazon"]
+    }
+  ] 
+
+  export default movie;
