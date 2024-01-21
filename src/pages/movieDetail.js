3243b490e7f174adeb68a3f39a02f43be63f9caa
hyunkyu-Park <park620630@gmail.com>
2024-01-21 16:57:45 -0500

    commit

--- src/pages/movieDetail.js
+++ src/pages/movieDetail.js
@@ -1,17 +1,32 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { Navigate, useNavigate, useParams } from "react-router-dom";
+import { Button } from "react-bootstrap";
 
 function MovieDetail(props) {
   let { id } = useParams();
+  let navigate = useNavigate();
 
   return (
-    <div className="wrapper">
-      <div className="movie_detail">
-        <img src={props.images[id]} width="60%" />
-        <h4>{props.movies[id].title}</h4>
-        <p className="description">{props.movies[id].content}</p>
+    <>
+      <div className="wrapper">
+        <div className="movie_detail">
+          <img src={props.images[id]} width="60%" />
+          <h4>{props.movies[id].title}</h4>
+          <p className="description">{props.movies[id].content}</p>
+        </div>
       </div>
-    </div>
+
+      <div>
+        <Button
+          variant="outline-primary"
+          onClick={() => {
+            navigate(-1);
+          }}
+        >
+          Back Button
+        </Button>{" "}
+      </div>
+    </>
   );
 }
 
