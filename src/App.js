b9eb620bf3bd5440ec46765f6ca4ee6f527d40c9
hyunkyu-Park <park620630@gmail.com>
2024-02-04 17:16:50 -0500

    movie ranking

--- src/App.js
+++ src/App.js
@@ -15,14 +15,20 @@ import MovieDetail from "./pages/movieDetail.js";
 import axios from "axios";
 
 function App() {
+  const apiKey = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxOWYyMGM3ZDVhMzA0NmExYWY1ZGE2Y2MxYzgwZDIzMCIsInN1YiI6IjY1YmYxMWFiYTdlMzYzMDFiNzU1OWYzNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.qnPVZ3E8x4LT0U7eYATrv7Ki9qVk2DNogJzOReqTZjU';
+
   let [images] = useState([m1, m2, m3]);
   let [movies, setMovies] = useState(movie);
+  let [timer, setTimer] = useState(false);
+
   let [review_images] = useState([review1, review2, review3]);
   let [reviews, setReviews] = useState(review);
-  let [timer, setTimer] = useState(false);
 
   let [postname, setPostname] = useState([]);
   let [input, setInput] = useState("");
+  const [movie_ranking, setMovieRanking] = useState([]);
+  const [error, setError] = useState(null);
+  let [year, setYear] = useState(2017)
 
   useEffect(() => {
     let timer = setTimeout(() => {
@@ -33,6 +39,36 @@ function App() {
     };
   }, []);
 
+  const handleClick = (year) => {
+    
+    axios.get('https://api.themoviedb.org/3/discover/movie', {
+      params: {
+        api_key: apiKey,
+        language: 'en-US',
+        sort_by: 'popularity.desc',
+        include_adult: true,
+        include_video: false,
+        page: 1,
+        primary_release_year: year,
+        region: 'US',
+      },
+      headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxOWYyMGM3ZDVhMzA0NmExYWY1ZGE2Y2MxYzgwZDIzMCIsInN1YiI6IjY1YmYxMWFiYTdlMzYzMDFiNzU1OWYzNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.qnPVZ3E8x4LT0U7eYATrv7Ki9qVk2DNogJzOReqTZjU', // 여기에 실제 액세스 토큰을 넣어주세요.
+      }
+    })
+      .then((response) => {
+        setMovieRanking(response.data.results);
+        setError(null);
+      })
+      .catch((error) => {
+        console.error(error);
+        setMovieRanking([]);
+        setError('영화 정보를 가져오는 중에 오류가 발생했습니다.');
+      });
+  };
+  
+
   return (
     <div className="App">
       <Navbar bg="primary" data-bs-theme="dark">
@@ -41,7 +77,7 @@ function App() {
           <Nav className="me-auto">
             <Nav.Link href="/future">Future</Nav.Link>
             <Nav.Link href="/past">Past</Nav.Link>
-            <Nav.Link href="/Review">Review</Nav.Link>
+            <Nav.Link href="/search">Search</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
@@ -81,7 +117,7 @@ function App() {
         />
 
         <Route
-          path="/review"
+          path="/search"
           element={
             <>
               <div>
@@ -138,6 +174,22 @@ function App() {
                 >
                   버튼2
                 </button>
+
+                <div>
+                  <h1>2017년 미국에서 가장 인기 있었던 영화</h1>
+                  <button onClick={() => handleClick(year)}>
+                    버튼3
+                  </button>
+                  {error && <p style={{ color: 'red' }}>{error}</p>}
+                  <ul>
+                    {movie_ranking.map(movie => (
+                      <li key={movie.id}>
+                        {movie.title}
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              
               </div>
             </>
           }
